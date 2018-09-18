74b8ccdeb907934b603a994d76dd292fd511446d
salamaAmoyo <salama.amoyo@gmail.com>
2018-09-18 15:01:45 +0300

    Add files via upload

---
+++ js file/js.js
@@ -0,0 +1,26 @@
+﻿function Dotable(x,y,color)
+{
+   var string="<table>";
+
+   for (i=1;i<=y ;i++ )
+   {
+     string+="<tr>";
+
+	   for (z=1;z<=x ;z++ )
+	   {
+	    string+="<td style='border:5px solid "+color+"'>X</td>";
+	   }
+
+     string+="</tr>";
+   }
+   string+="</table><br>";
+
+
+   document.write(string);
+}
+
+
+Dotable(10,10,"green");
+Dotable(5,5,"blue");
+Dotable(8,8,"yellow");
+
---
+++ js file/salama.js
@@ -0,0 +1,18 @@
+﻿<DOCTYPE html>
+<html>
+<head>
+<script src="js.js"></script>
+</head>
+<body>
+
+<h1>helo salama<h1>
+
+
+
+
+
+
+
+
+</body>
+</html>
